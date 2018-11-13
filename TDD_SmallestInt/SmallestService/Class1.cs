49a60d5aac30b808c454c90783d0d3f020610d7d
Michael <mikeishere3@gmail.com>
2018-11-12 19:36:54 -0500

    smallestIntTDD done, DueDateTDD refactored

--- TDD_SmallestInt/SmallestService/Class1.cs
+++ TDD_SmallestInt/SmallestService/Class1.cs
@@ -12,5 +12,18 @@ namespace SmallestService
             Array.Sort(inputArray);
             return inputArray[0];
         }
+
+        public int FindClosestIntToZero(int[] input)
+        {
+            if (input.Length > 1)
+            {
+                Array.Sort(input);
+                var i = 0;
+                if (Math.Abs(input[i]) > Math.Abs(input[i + 1])) i++;
+                return input[i];
+            }
+            else if (input.Length == 1) return input[0];
+            throw new System.ArgumentException();
+        }
     }
 }
